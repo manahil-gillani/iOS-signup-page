a51451298d68e18b5e6cb61ef9ab2755f5d0f014
Manahil Fatima <125747158+manahil-gillani@users.noreply.github.com>
2024-07-20 17:40:12 +0500

    folder change

--- registration-form/ViewController.swift
+++ registration-form/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //UI labels
+    @IBOutlet weak var passmatch: UILabel!
+    @IBOutlet weak var successmsg: UILabel!
+    //UI text fields
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirm: UITextField!
+    //UI buttons
+    @IBOutlet weak var signupbutton: UIButton!
+    @IBOutlet weak var loginbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        clearall()
     }
 
+    func clearall(){
+        passmatch.text=""
+        successmsg.text=""
+    }
+    
+    @IBAction func signupbutton(_ sender: Any) {
+        guard let usernames = username.text, !usernames.isEmpty,
+              let emails = email.text, !emails.isEmpty,
+              let passwords = password.text, !passwords.isEmpty,
+              let confirms = confirm.text, !confirms.isEmpty
+              else
+        {
+            showAlert(message: "please fill all the fields.")
+            return
+        }
+        // Ensure passwords match
+               if passwords != confirms {
+                   passmatch.text = "Passwords do not match."
+                   return
+               } else {
+                   passmatch.text = ""
+               }
 
+               // Proceed with signup logic
+               successmsg.text = "Signup successful!"
+    }
+    
+    func showAlert(message: String) {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    @IBAction func loginbutton(_ sender: Any) {
+    }
+    
 }
 
